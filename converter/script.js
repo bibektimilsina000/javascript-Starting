551d4d1eae60e29aa73e2155d05529a2df50b30f
Prashant8Khatiwada <98466046+Prashant8Khatiwada@users.noreply.github.com>
2022-06-02 22:00:32 +0545

    Add files via upload

---
+++ converter/script.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", toFahrenheit);
+
+function toFahrenheit(){
+    doucment.getElementById("converter").innerHTML = "<P>Convert To Fahrenheit</P>";
+    console.log("clicked");
+}

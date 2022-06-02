f345cf41b6e96b03f77837f2e1181bf1f27efb0e
bibektimilsina000 <96974370+bibektimilsina000@users.noreply.github.com>
2022-06-02 22:29:03 +0545

    Delete converter directory

--- converter/script.js
+++
@@ -1,7 +0,0 @@
-let btn1 = document.getElementById("btn1");
-btn1.addEventListener("click", toFahrenheit);
-
-function toFahrenheit(){
-    doucment.getElementById("converter").innerHTML = "<P>Convert To Fahrenheit</P>";
-    console.log("clicked");
-}
